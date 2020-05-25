9a583ee13b09feef95f7b76bd3a3aba8618616f4
heathernfran <heathernfran@gmail.com>
2020-05-24 20:16:22 -0700

    Add mixins for centering, and scrolling content

---
+++ src/styles/mixins.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components'
+
+const centeredFlex = css`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`
+
+const scrollable = css`
+  height: 100%;
+  overflow-y: auto;
+`
+
+export { centeredFlex, scrollable }
